36d74bdaf456ad7fdfb5f622a0bdcd5d9c60a1e2
John Francis <jean_f_05@hotmail.com>
2022-04-20 13:18:25 +0100

    first commit

---
+++ src/js/controller.js
@@ -0,0 +1,78 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import * as model from './model';
+import recipeView from './views/recipeView';
+import searchView from './views/searchView';
+import resultsView from './views/resultsView';
+import paginationView from './views/paginationView';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.substring(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    //UPDATE RESULTS VIEW TO MARK SELECTED SEARCH RESULT
+    resultsView.update(model.getSearchResultsPage());
+
+    //LOADING RECIPE
+    await model.loadRecepie(id);
+
+    //RENDER RECIPE
+    recipeView.render(model.state.recipe);
+  } catch (error) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    //GET SEARCH QUERY
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //LOAD SEARCH RESULTS
+    await model.LoadSearchResults(query);
+    //RENDER RESULTS
+
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    //RENDER INITIAL PAGINATION BUTTONS
+    paginationView.render(model.state.search);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  //RENDER NEW RESULTS
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  //RENDER NEW PAGINATION BUTTONS
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  //UPDATE RECIPE SERVINGS (IN STATE)
+  model.updateServings(newServings);
+  //UPDATE RECIPE VIEW
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+
+init();
---
+++ src/js/model.js
@@ -0,0 +1,69 @@
+import { API_URL, RES_PER_PAGE } from './config';
+import { getJSON } from './helpers.js';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+    resultsPerPage: RES_PER_PAGE,
+    page: 1,
+  },
+};
+
+export const loadRecepie = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}${id}`);
+    const { recipe } = data.data;
+
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (error) {
+    console.error(`${error} ERRORRRRR`);
+    throw error;
+  }
+};
+
+export const LoadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (error) {
+    console.error(`${error} ERRORRRRR`);
+    throw error;
+  }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage; //0
+  const end = page * state.search.resultsPerPage; //9
+
+  return state.search.results.slice(start, end);
+};
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    // newQt = oldQt * newServings / oldServings
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+  });
+  state.recipe.servings = newServings;
+};
